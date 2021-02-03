a80ddbdeacf8cb7ed096ac8644ccb530783c6555
InstantlyMoist <kyllian007@gmail.com>
2021-02-03 11:37:14 +0100

    Fixed typo causing new function not to work

--- src/main/java/me/kyllian/captcha/spigot/handlers/CaptchaHandler.java
+++ src/main/java/me/kyllian/captcha/spigot/handlers/CaptchaHandler.java
@@ -51,7 +51,7 @@ public class CaptchaHandler {
                 || player.hasPermission("captcha.override") && plugin.getConfig().getBoolean("captcha-settings.permission-override"))
             return;
         player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, Integer.MAX_VALUE, 255));
-        int heldSlot = plugin.getConfig().getInt("captch-settings.held-slot");
+        int heldSlot = plugin.getConfig().getInt("captcha-settings.held-slot");
         if (heldSlot != -1) player.getInventory().setHeldItemSlot(heldSlot);
         Captcha captcha = captchaFactory.getCaptcha(player);
         playerData.setAssignedCaptcha(captcha);
